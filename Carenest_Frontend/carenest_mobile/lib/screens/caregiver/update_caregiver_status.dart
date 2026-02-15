97416c41ad6ce18cafa195623e422e1a6ec2e122
AyumiKavindya <ayumikavi@gmail.com>
2026-02-15 19:58:59 +0530

    Update caregiver update  page with custom theme

--- Carenest_Frontend/carenest_mobile/lib/screens/caregiver/update_caregiver_status.dart
+++ Carenest_Frontend/carenest_mobile/lib/screens/caregiver/update_caregiver_status.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:carenest_mobile/core/app_theme.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
@@ -63,18 +64,15 @@ class _UpdateCareStatusPageState extends State<UpdateCareStatusPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.green,
-        title: const Text(
-          'Update Care Status',
-          style: TextStyle(
-            fontWeight: FontWeight.bold,
-            fontSize: 22,
-            color: Colors.white,
-          ),
-        ),
+        backgroundColor: AppTheme.primaryDark,
         centerTitle: true,
+        title: Text(
+          'Update Care Status',
+          style: AppTheme.headingLarge.copyWith(color: Colors.black),
+        ),
+        //centerTitle: true,
         leading: IconButton(
-          icon: const Icon(Icons.arrow_back, color: Colors.white),
+          icon: const Icon(Icons.arrow_back, color: Colors.black),
           onPressed: () {
             Navigator.pop(context); // go back to previous screen
           },
@@ -98,12 +96,8 @@ class _UpdateCareStatusPageState extends State<UpdateCareStatusPage> {
                 height: 120, // increase height
                 child: Card(
                   elevation: 3, //shadow
-                  color: const Color.fromARGB(
-                    255,
-                    192,
-                    239,
-                    140,
-                  ), // backgroundd color
+                  color: AppTheme.softGreen,
+                  // backgroundd color
                   shape: RoundedRectangleBorder(
                     borderRadius: BorderRadius.circular(10),
                   ),
@@ -116,52 +110,43 @@ class _UpdateCareStatusPageState extends State<UpdateCareStatusPage> {
                     leading: const Icon(Icons.person, size: 40),
                     title: Text(
                       widget.patientName,
-                      style: const TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 16,
-                        color: Colors.black,
-                      ),
+                      style: AppTheme.headingMedium,
                     ),
 
                     subtitle: Text(
                       'Visit ID: ${widget.visitId}',
-                      style: const TextStyle(color: Colors.black54),
+                      style: AppTheme.bodyText,
                     ),
                   ),
                 ),
               ),
             ),
+
             const SizedBox(height: 30),
 
             // Status Dropdown
-            const Text(
-              'Care Status',
-              style: TextStyle(fontSize: 16, fontWeight: FontWeight.w800),
-            ),
+            Text('Care Status', style: AppTheme.headingMedium),
             const SizedBox(height: 14),
+
+            //Status dropdown
             DropdownButtonFormField<String>(
               value: selectedStatus,
               hint: const Text('Select status'),
               items: statusList.map((status) {
-                return DropdownMenuItem(value: status, child: Text(status));
+                return DropdownMenuItem<String>(
+                  value: status,
+                  child: Text(status, style: AppTheme.bodyText),
+                );
               }).toList(),
               onChanged: (value) {
                 setState(() => selectedStatus = value);
               },
-              decoration: InputDecoration(
+              decoration: const InputDecoration(
                 hintText: 'Select Status',
-                enabledBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10),
-                  borderSide: BorderSide(
-                    color: Colors.lightGreen, // normal border color
-                    width: 2,
-                  ),
-                ),
-
-                focusedBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10),
-                  borderSide: BorderSide(
-                    color: Colors.blue, // focused border color
+                border: OutlineInputBorder(
+                  borderRadius: const BorderRadius.all(Radius.circular(10)),
+                  borderSide: const BorderSide(
+                    color: Colors.lightGreen,
                     width: 2,
                   ),
                 ),
@@ -210,24 +195,11 @@ class _UpdateCareStatusPageState extends State<UpdateCareStatusPage> {
           width: double.infinity,
           height: 50,
           child: ElevatedButton(
+            style: ElevatedButton.styleFrom(backgroundColor: AppTheme.primary),
             onPressed: isLoading ? null : updateCareStatus,
-            style: ElevatedButton.styleFrom(
-              backgroundColor: Colors.green, // <-- change your color here
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(8),
-                // optional: rounded corners
-              ),
-            ),
             child: isLoading
                 ? const CircularProgressIndicator(color: Colors.white)
-                : const Text(
-                    'Update Status',
-                    style: TextStyle(
-                      fontSize: 16,
-                      color: Colors.white,
-                      fontWeight: FontWeight.w800,
-                    ),
-                  ),
+                : Text('Update Status', style: AppTheme.buttonText),
           ),
         ),
       ),
