cfbcabcc6a79734fb394eee90f2c46f0805d4ee4
Ayumi@2044$ <ayumikavi@gmail.com>
2026-03-21 21:53:39 +0530

    Add test 1: request caregiver page shows title

---
+++ Carenest_Frontend/carenest_mobile/test/request_caregiver_page_test.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter/services.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+// ✅ CORRECT IMPORT
+import 'package:carenest_mobileapp/screens/patient/request_caregiver.dart';
+
+void main() {
+  TestWidgetsFlutterBinding.ensureInitialized();
+
+  setUpAll(() async {
+    const MethodChannel channel =
+        MethodChannel('plugins.flutter.io/shared_preferences');
+
+    channel.setMockMethodCallHandler((MethodCall methodCall) async {
+      return {};
+    });
+
+    await Supabase.initialize(
+      url: 'https://xyzcompany.supabase.co',
+      anonKey: 'public-anon-key',
+    );
+  });
+
+  /// ✅ TEST 1
+  testWidgets('Request care page builds and shows title',
+      (WidgetTester tester) async {
+    await tester.pumpWidget(
+      const MaterialApp(
+        home: RequestCarePage(),
+      ),
+    );
+
+    await tester.pump();
+
+    expect(find.text('Request Care'), findsOneWidget);
+  });
+}
