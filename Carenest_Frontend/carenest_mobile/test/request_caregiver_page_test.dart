9d843e688f947bddbc9adcc2ed7d317cca6839fe
Ayumi@2044$ <ayumikavi@gmail.com>
2026-03-21 21:58:43 +0530

    Add test 2: service type section displayed

--- Carenest_Frontend/carenest_mobile/test/request_caregiver_page_test.dart
+++ Carenest_Frontend/carenest_mobile/test/request_caregiver_page_test.dart
@@ -2,8 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter/services.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
-
-// ✅ CORRECT IMPORT
 import 'package:carenest_mobileapp/screens/patient/request_caregiver.dart';
 
 void main() {
@@ -35,5 +33,19 @@ void main() {
     await tester.pump();
 
     expect(find.text('Request Care'), findsOneWidget);
+  });
+
+  /// ✅ TEST 2
+  testWidgets('Service Type section is displayed',
+      (WidgetTester tester) async {
+    await tester.pumpWidget(
+      const MaterialApp(
+        home: RequestCarePage(),
+      ),
+    );
+
+    await tester.pump();
+
+    expect(find.text('Service Type'), findsOneWidget);
   });
 }
