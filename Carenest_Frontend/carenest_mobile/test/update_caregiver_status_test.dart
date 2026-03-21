29382ceff7cd280c3653274d5e0409fd68056a95
Ayumi@2044$ <ayumikavi@gmail.com>
2026-03-21 22:40:29 +0530

    Add test 2: care notes section displayed

--- Carenest_Frontend/carenest_mobile/test/update_caregiver_status_test.dart
+++ Carenest_Frontend/carenest_mobile/test/update_caregiver_status_test.dart
@@ -2,8 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter/services.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
-
-// ✅ CORRECT IMPORT
 import 'package:carenest_mobileapp/screens/caregiver/update_caregiver_status.dart';
 
 void main() {
@@ -35,5 +33,19 @@ void main() {
     await tester.pump();
 
     expect(find.text('Complete Session'), findsOneWidget);
+  });
+
+  /// ✅ TEST 2
+  testWidgets('Care Notes section is displayed',
+      (WidgetTester tester) async {
+    await tester.pumpWidget(
+      const MaterialApp(
+        home: UpdateCareStatusPage(),
+      ),
+    );
+
+    await tester.pump();
+
+    expect(find.text('Care Notes (Required)'), findsOneWidget);
   });
 }
