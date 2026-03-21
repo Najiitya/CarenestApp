ef0393e10bff89bd8bd3b178a33ef9dc8fb432f4
Adeesha Sandamin <adeesha.20241147@iit.ac.lk>
2026-03-21 16:42:27 +0530

    Add test 1: patient page builds without crashing

---
+++ Carenest_Frontend/carenest_mobile/test/patient_detail_test.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter/services.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:carenest_mobileapp/screens/patient/patient_details.dart';
+
+void main() {
+  TestWidgetsFlutterBinding.ensureInitialized();
+
+  setUpAll(() async {
+    const MethodChannel channel =
+        MethodChannel('plugins.flutter.io/shared_preferences');
+
+    channel.setMockMethodCallHandler((MethodCall methodCall) async {
+      return {};
+    });
+
+    await Supabase.initialize(
+      url: 'https://xyzcompany.supabase.co',
+      anonKey: 'public-anon-key',
+    );
+  });
+
+  testWidgets('Patient page builds without crashing',
+      (WidgetTester tester) async {
+    await tester.pumpWidget(
+      const MaterialApp(
+        home: PatientDetailsPage(),
+      ),
+    );
+
+    await tester.pump();
+
+    expect(find.byType(MaterialApp), findsOneWidget);
+  });
+}
